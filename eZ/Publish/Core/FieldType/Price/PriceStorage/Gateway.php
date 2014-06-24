a3a4855d5fa54d13112b869da11ea4a712471b48
crevillo <crevillo@gmail.com>
2014-06-24 22:14:30 +0200

    Refactored the Gateway to start only with the getData from the Storage part. Also add the mehtod for getting data from the legacy storage

--- eZ/Publish/Core/FieldType/Price/PriceStorage/Gateway.php
+++ eZ/Publish/Core/FieldType/Price/PriceStorage/Gateway.php
@@ -19,31 +19,11 @@ use eZ\Publish\SPI\Persistence\Content\Field;
 abstract class Gateway extends StorageGateway
 {
     /**
-     * Stores a price based on the given field data
-     *
-     * @param VersionInfo $versionInfo
-     * @param Field $field
-     *
-     * @return boolean
-     */
-    abstract public function storeFieldData( VersionInfo $versionInfo, Field $field );
-
-    /**
      * Get the price data, if there is a price for the given field
      *
      * @param Field $field
      *
      * @return void
      */
-    abstract public function getFieldData( Field $field );
-
-    /**
-     * Deletes external data for $fieldId in $versionNo
-     *
-     * @param mixed $fieldId
-     * @param mixed $versionNo
-     *
-     * @return void
-     */
-    abstract public function deleteFieldData( $versionNo, $fieldId );
+    abstract public function getPriceInfo( Field $field );
 }
--- eZ/Publish/Core/FieldType/Price/PriceStorage/Gateway/LegacyStorage.php
+++ eZ/Publish/Core/FieldType/Price/PriceStorage/Gateway/LegacyStorage.php
@@ -10,8 +10,7 @@
 namespace EzSystems\EzPriceBundle\eZ\Publish\Core\FieldType\Price\PriceStorage\Gateway;
 
 use eZ\Publish\Core\Persistence\Database\DatabaseHandler;
-use eZ\Publish\Core\FieldType\Url\UrlStorage\Gateway;
-use eZ\Publish\SPI\Persistence\Content\VersionInfo;
+use \EzSystems\EzPriceBundle\eZ\Publish\Core\FieldType\Price\PriceStorage\Gateway;
 use eZ\Publish\SPI\Persistence\Content\Field;
 
 /**
@@ -62,46 +61,26 @@ class LegacyStorage extends Gateway
    }
 
    /**
-    * @see \EzSystems\EzPriceBundle\eZ\Publish\Core\FieldType\Price\PriceStorage\Gateway
-    */
-   public function storeFieldData( VersionInfo $versionInfo, Field $field )
-   {
-
-   }
-
-   /**
     * Gets the price for the given field
     *
     * @see \EzSystems\EzPriceBundle\eZ\Publish\Core\FieldType\Price\PriceStorage\Gateway
     */
-   public function getFieldData( Field $field )
+   public function getPriceInfo( Field $field )
    {
-       $field->value->externalData = $this->fetchPrice( $field->id, $field->versionNo );
-   }
-
-   /**
-    * Deletes price data for field with $fieldId in $versionNo.
-    *
-    * @param mixed $fieldId
-    * @param mixed $versionNo
-    *
-    * @return void
-    */
-   public function deleteFieldData( $fieldId, $versionNo )
-   {
+       $field->value->externalData = $this->fetchPriceData( $field->id, $field->versionNo );
    }
 
    /**
     * Query database for getting the price associated to the fieldId and
     * versionNo
-    * Also get vat type used and it vat is excluded or included in the base price
+    * Also get vat percentage to apply and if vat is excluded or included in the base price
     *
     * @param int $fieldId
     * @param int $versionNo
     *
     * @return array
     */
-    private function fetchPrice( $fieldId, $versionNo )
+    private function fetchPriceData( $fieldId, $versionNo )
     {
         $price = array();
         $dbHandler = $this->getConnection();
@@ -131,11 +110,8 @@ class LegacyStorage extends Gateway
         // get data_text and explode it
         $rowDataText = explode( ',', $priceRow['data_text'] );
 
-        // vat type id is in the first position of the exploded data_text
-        $price['selected_vat_type'] = $rowDataText[0];
-
         // is_vat_included is in the second position. Convert to boolean here
-        // depending on the value
+        // depending on this value
         $price['is_vat_included'] = $rowDataText[1] == 1 ? true : false;
 
         // vat_percengage depending of the vat type selected
