b7283587a427abc4d9948ad443be588d5d626479
crevillo <crevillo@gmail.com>
2014-06-25 09:57:38 +0200

    Refactored constructor and redo some tests

--- eZ/Publish/Core/FieldType/Price/Value.php
+++ eZ/Publish/Core/FieldType/Price/Value.php
@@ -7,18 +7,40 @@ use eZ\Publish\Core\FieldType\Value as BaseValue;
 class Value extends BaseValue
 {
     /**
+     * Base price
+     *
      * @var float
      */
     public $price;
 
     /**
+     * Is the VAT included with the price or not
+     *
+     * @var bool
+     */
+    public $is_vat_included = false;
+
+    /**
+     * Percentage associated with the VAT
+     *
+     * @var float
+     */
+    public $vat_percentage = 0;
+
+    /**
      * Construct a new Value object and initialize with $value
      *
-     * @param float|null $value
+     * @param array|null $value
      */
     public function __construct( $value = null )
     {
-        $this->price = $value;
+        if ( $value !== null )
+        {
+            foreach ( $value as $key => $val )
+            {
+                $this->$key = $val;
+            }
+        }
     }
 
 
