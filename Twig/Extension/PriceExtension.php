2b50b97fcb19329cc965d5c4c6a765948de6757c
crevillo <crevillo@gmail.com>
2014-06-26 22:03:01 +0200

    added Twig Extension class

---
+++ Twig/Extension/PriceExtension.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the EzPriceBundle package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+namespace EzSystems\EzPriceBundle\Twig\Extension\PriceExtension;
+
+use Twig_Extension;
+
+class PriceExtension extends Twig_Extension
+{
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return "ezprice";
+    }
+}
